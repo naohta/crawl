45d6706bd383700348cfe3cad29d843ebfc1eaca
Naohiro OHTA <naohta@gmail.com>
2013-03-07 00:39:16 +0900

    Ya!

--- retrieve.rb
+++ retrieve.rb
@@ -2,6 +2,8 @@
 require 'net/http'
 require 'digest/md5'
 require 'lhalib'
+require 'nkf'
+require 'shell'
 
 DOWNLOAD_DIR = "downloaded/"
 Net::HTTP.version_1_2
@@ -17,8 +19,18 @@ if(!File.exist?(DOWNLOAD_DIR)) then Dir.mkdir(DOWNLOAD_DIR) end
 File.open(file_name,"wb") do |f|
   f.print res.body
 end
-
-LhaLib::x(file_name)
-#LhaLib::x("a.lzh")
 puts Digest::MD5.file(file_name).hexdigest
 
+LhaLib::x(file_name) do |info|
+  puts info
+  puts NKF.guess info[:name]
+  system "mv #{info[:name]} #{DOWNLOAD_DIR}kihon_bu.xls"
+end
+
+cmd = "echo hi!"
+`#{cmd}`
+
+system "ls"
+p "yay!"
+exec "#{cmd}"
+p "wow!"
